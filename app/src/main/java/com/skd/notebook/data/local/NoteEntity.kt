5eb3df9639c4f8d0b98c0481acfbaf8a09c2b888
saruj <14skd12@gmail.com>
2026-03-10 13:08:41 +0530

    changes entity code

--- app/src/main/java/com/skd/notebook/data/local/NoteEntity.kt
+++ app/src/main/java/com/skd/notebook/data/local/NoteEntity.kt
@@ -2,7 +2,7 @@ package com.skd.notebook.data.local
 
 import androidx.room.Entity
 import androidx.room.PrimaryKey
-import java.util.UUID
+
 @Entity(tableName = "notes")
 data class NoteEntity(
     @PrimaryKey
