cfe08c192b4beefa731f0f124cde5468848a6d76
saruj <14skd12@gmail.com>
2026-05-14 22:00:24 +0530

    table create

--- app/src/main/java/com/skd/notebook/data/local/NoteEntity.kt
+++ app/src/main/java/com/skd/notebook/data/local/NoteEntity.kt
@@ -17,7 +17,6 @@ import com.google.firebase.firestore.IgnoreExtraProperties
 @Entity(tableName = "notes")
 data class NoteEntity(
     @PrimaryKey var id: String = "",
-
     var title: String = "",
     var description: String = "",
     var timestamp: Long = 0L,
