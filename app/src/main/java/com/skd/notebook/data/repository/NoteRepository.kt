2884d2bcb7454fb7395b4a5fb4a82e2d47119f7d
saruj <14skd12@gmail.com>
2026-09-10 22:23:43 +0530

    repo change..... space

--- app/src/main/java/com/skd/notebook/data/repository/NoteRepository.kt
+++ app/src/main/java/com/skd/notebook/data/repository/NoteRepository.kt
@@ -17,8 +17,7 @@ class NoteRepository(
     val archivedNotes= noteDao.getArchivedNotes()
     val pinnedNotes  = noteDao.getPinnedNotes()
     val folders      = folderDao.getAllFolders()
-
-
+    
     fun getFolderNotes(folderId: String) = noteDao.getNotesByFolder(folderId)
     fun searchNotes(query: String)      = noteDao.searchNotes(query)
     suspend fun getNoteById(id: String) = noteDao.getNoteById(id)
