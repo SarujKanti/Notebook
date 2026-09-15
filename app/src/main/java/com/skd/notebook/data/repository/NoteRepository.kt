b8c6acf67e3803968134aa30a7f675a8eb197a1c
saruj <14skd12@gmail.com>
2026-09-15 21:41:57 +0530

    repo change..... space

--- app/src/main/java/com/skd/notebook/data/repository/NoteRepository.kt
+++ app/src/main/java/com/skd/notebook/data/repository/NoteRepository.kt
@@ -33,7 +33,6 @@ class NoteRepository(
         trySync { firebase.saveNote(note) }
     }
 
-
     /** Soft-delete: moves note to Bin */
     suspend fun moveToBin(note: NoteEntity) {
         val updated = note.copy(isDeleted = true, deletedAt = System.currentTimeMillis(), isArchived = false)
